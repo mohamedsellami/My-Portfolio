f61ab4b11a9eb5c504a3c06d8de9e4ae70ab8495
Mohamed-Sellami <64484505+mohamedsellami@users.noreply.github.com>
2021-01-07 19:50:36 +0100

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -14,7 +14,8 @@ function Contact() {
             </Link>
             <div className="contact-container">
                 <h2>Contact me</h2>
-                <form id="contact-form" name="contact" method="POST" action="/" data-netlify="true">
+                <form id="contact-form" name="contact" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact-value" />
                     <input name="email" className="form-control email" type="email" placeholder="Your email" required/>
                     <br/>
                     <textarea name="message" className="form-control" placeholder="Message" col="10" required/>
